ff8d52d5c76695d46747c4743d374d65d8ec8770
Sweety Kumari <sweetykumari98222gmail.com>
2022-11-19 22:12:36 +0530

    first commit

---
+++ grocery/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import CouponCode
+from .models import *
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Booking)
+admin.site.register(Profile)
+admin.site.register(Send_Feedback)
+admin.site.register(Status)
+admin.site.register(Cart)
+admin.site.register(CouponCode)
